e14d7f87501579ba6791dca6e65208a97ac91599
David Estepa <david.estepa98@gmail.com>
2023-08-02 09:46:25 -0500

    Added components related with UI and Styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React from "react"
+
+import UsersList from '../components/UsersList'
 
 const Users = () => {
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Don Santo',
+            image:
+              'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/54.svg',
+            places: 3
+          
+        }
+    ]
+
     return (
-        <h1>
-            Users Work!!
-        </h1>
+        <UsersList items={USERS} />
     )
 }
 
